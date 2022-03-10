dd15ec7fbc6672ee020e5dbebd925d8671954b87
Seongbink <tjdqls2s@naver.com>
2022-03-10 22:25:29 +0900

    14 최상위클래스

---
+++ JavaStudy_9432/src/a14_최상위클래스/Computer.java
@@ -0,0 +1,15 @@
+package a14_최상위클래스;
+
+public class Computer {
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "강성빈";
+	}
+	
+	public void computerTest() {
+		System.out.println("컴퓨터 TEST 메소드");
+	}
+
+}
