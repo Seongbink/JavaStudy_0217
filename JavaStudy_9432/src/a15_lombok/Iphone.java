25882f71ff35462a1e9a4158353829708a3979d7
Seongbink <tjdqls2s@naver.com>
2022-03-11 12:53:57 +0900

    a15 롬복 수정본

---
+++ JavaStudy_9432/src/a15_lombok/Iphone.java
@@ -0,0 +1,29 @@
+package a15_lombok;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+//@Getter
+//@Setter
+//@ToString
+//@EqualsAndHashCode 4개가 Data 랑 같음
+@Data
+
+public class Iphone {
+	private int series;
+	private String model;
+	private String color;
+	private int memorySize;
+	private int releaseYear;
+	
+}
